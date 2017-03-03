be00eafa467ec6f4c58800db7805cf5c9c6f2ca2
Cheri Allen <cherimarie@gmail.com>
2017-03-02 20:50:05 -0800

    Code from class

--- scripts.js
+++ scripts.js
@@ -1,9 +1,52 @@
-alert("Let's get to work! ")
-// TODO: Build an array of strings that could be answers to magic 8 ball type questions.
+// This section should be very similar to what you'll write 
+// for adding/removing items to cart in homework
+var cart = [{name: "yes"}, {name: "no"}]
 
+function clicker(fortune){
+  
+  var i = cart.findIndex(function(el){
+    return el.name.toLowerCase() == fortune 
+  });
+  
+  // if i >= 0 
+  // splice to get it out of the array
+  // else 
+  // push it into the array
+  
+  console.log(i);
+}
 
-/* TODO: Write a form handler function that will:
-  1. capture the question asked in the form
-  2. randomly select an element from the array of answers
-  3. console log something conversational to user, like "Oh, you want to know [question]? Well, I think... [answer]"
-*/
+// More general add, removal, and retrieval from arrays
+var fortunes = ["yes", "no", "only on full moons"];
+
+function removeAnswer(){
+  var remv = document.removalForm.remove.value;
+  
+  var index = fortunes.indexOf(remv);
+  
+  if(index >= 0){
+    fortunes.splice(index, 1)
+  }
+  console.log(fortunes); 
+  event.preventDefault();
+}
+
+function insertAnswer(){
+  var answer = document.answerForm.answer.value;
+  fortunes.push(answer);
+  console.log(fortunes);
+  event.preventDefault(); 
+}
+
+function magicAnswer(){
+  var q = document.questionForm.question.value;
+  // We're just copying this rando code from the book :) 
+  var rando = Math.floor(Math.random() * fortunes.length);
+  var answer = fortunes[rando];
+  var output = "So you want to know " + q + "... Well I think... " + answer;
+  
+  console.log(output);
+  
+  event.preventDefault();
+}
+
